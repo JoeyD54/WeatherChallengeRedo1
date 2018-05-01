3f2b55a83ffd642e29802e096bb3c427d4afb128
Joey Domino <playdude224@hotmail.com>
2018-05-01 19:03:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.playd.weatherchallengeredo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+     TODO(1) Create single page app to fetch and display weather conditions using users location
+        use  https://www.wunderground.com/weather/api/d/docs?d=data/conditions
+     */
+}
