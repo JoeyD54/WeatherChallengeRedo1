fdc0579ad9ce5eb5cba7c5cc911998bcb5ced28e
Joey Domino <playdude224@hotmail.com>
2018-05-02 18:21:16 -0400

    messing with Google fusedLocationProviderClient

--- app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
+++ app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.playd.weatherchallengeredo1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
     /*
      TODO(1) Create single page app to fetch and display weather conditions using users location
         use  https://www.wunderground.com/weather/api/d/docs?d=data/conditions
+
+        condUrl = "http://api.wunderground.com/api/90645b02d360fe14/conditions/q/" + state + "/" + city + ".json";
+
      */
 }
