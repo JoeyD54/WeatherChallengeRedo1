23ba9c10f12ba1097813da2257d3799a7f6247f7
Joey Domino <playdude224@hotmail.com>
2018-05-02 20:03:35 -0400

    First steps toward location request
    Set up text views and a button into main activity.
    
    Set up fusedLocationProviderClient from Google to help with location requests.
    
    Also started permission requests for COARSE location (general location service) to get city location and temperature. Currently not functioning fully.

--- app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
+++ app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.playd.weatherchallengeredo1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private TextView printCity, printTemperature;
+    private Button weatherButton;
+    private int MY_LOCATION_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        printCity = (TextView) findViewById(R.id.cityText);
+        printTemperature = (TextView) findViewById(R.id.temperatureText);
+        weatherButton = (Button) findViewById(R.id.weatherButton);
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-    }
+        askLocationPermission();
 
+        if (weatherButton.isPressed() && hasPermission()) {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                printCity.setText(location.toString());
+                            }
+                        }
+                    });
+        }
+    }
+-
     /*
      TODO(1) Create single page app to fetch and display weather conditions using users location
         use  https://www.wunderground.com/weather/api/d/docs?d=data/conditions
@@ -26,4 +55,42 @@ public class MainActivity extends AppCompatActivity {
         condUrl = "http://api.wunderground.com/api/90645b02d360fe14/conditions/q/" + state + "/" + city + ".json";
 
      */
+
+    public boolean hasPermission() {
+        if (ContextCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+    public void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Toast.makeText(this, "Location permission is needed to get accurate weather data.",
+                        Toast.LENGTH_SHORT).show();
+                askLocationPermission();
+            } else {
+                ActivityCompat.requestPermissions
+                        (this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_LOCATION_PERMISSION);
+            }
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == MY_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
