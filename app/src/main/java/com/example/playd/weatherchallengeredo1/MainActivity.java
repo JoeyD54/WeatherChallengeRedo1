4b03c60e88e4e197c9180c76bb79e58185c0ac57
Joey Domino <playdude224@hotmail.com>
2018-05-14 18:20:22 -0400

    Location Data receiving
    Getting Lat and Long. Now to reverse geocode to get address for API call.

--- app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
+++ app/src/main/java/com/example/playd/weatherchallengeredo1/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.playd.weatherchallengeredo1;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,27 +13,22 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements LocationListener{
 
     private TextView printCity, printTemperature;
     private Button weatherButton;
-    public final int MY_LOCATION_PERMISSION = 0;
-    public LocationManager locationManager;
-    public String provider;
+    public final int MY_LOCATION_PERMISSION = 100;
     boolean locationChecked;
 
     private FusedLocationProviderClient fusedLocationProviderClient;
@@ -44,53 +39,25 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        provider = locationManager.getBestProvider(new Criteria(), false);
-
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         printCity = (TextView) findViewById(R.id.cityText);
         printTemperature = (TextView) findViewById(R.id.temperatureText);
-        weatherButton = (Button) findViewById(R.id.weatherButton);
 
         locationChecked = checkLocationPermission();
 
-        //Setting intervals for location gathering to a min of 1 minute to a max of 5
-
-        Toast.makeText(this, "We're in the if statement now", Toast.LENGTH_LONG).show();
-
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(300000);
-        locationRequest.setFastestInterval(60000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-
-        SettingsClient client = LocationServices.getSettingsClient(this);
-        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+        weatherButton = (Button) findViewById(R.id.weatherButton);
+        weatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-
+            public void onClick(View v) {
+                if(locationChecked){
+                    //get location again somehow.
+                }
             }
         });
 
-//        buttonPressed();
-
-//        if (weatherButton.isPressed()) {
-//            printCity.setText(location.toString());
-//        }
     }
 
- /*   public void buttonPressed(Location location){
-        if(weatherButton.isPressed()){
-            printCity.setText(location.toString());
-
-        }
-    }
     /*
      TODO(1) Create single page app to fetch and display weather conditions using users location
         use  https://www.wunderground.com/weather/api/d/docs?d=data/conditions
@@ -98,9 +65,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         condUrl = "http://api.wunderground.com/api/90645b02d360fe14/conditions/q/" + state + "/" + city + ".json";
 
      */
-    //setup location interval and create location task. Other shit.
-    public void
-
 
     public boolean checkLocationPermission(){
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -147,14 +111,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                     if(ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_COARSE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED){
-                        locationManager.requestLocationUpdates(provider, 400, 1, this);
 
                         fusedLocationProviderClient.getLastLocation()
                                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                                     @Override
                                     public void onSuccess(Location location) {
                                         if(location != null){
-
+                                            //Do location logic here.
+                                            onLocationChanged(location);
                                         }
                                     }
                                 });
@@ -185,11 +149,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     }
 
     @Override
-    public void onLocationChanged(Location location){
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        String msg = "New latitude: " + latitude + " New longitude: " + longitude;
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    public void onLocationChanged(Location location) {
+        Double latitude = location.getLatitude();
+        Double longitude = location.getLongitude();
+        printCity.setText(Double.toString(latitude));
+        printTemperature.setText(Double.toString(longitude));
     }
 
+//    @Override
+//    protected void onHandleIntent(Intent intent){
+//        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+//    }
 }
